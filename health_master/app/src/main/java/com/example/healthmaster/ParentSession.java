5a4d1a2eb7b9485218868531683f19dcce47cace
tomalikem <58105407+tomalikem@users.noreply.github.com>
2020-02-29 17:57:42 +0100

    ParentModel

---
+++ health_master/app/src/main/java/com/example/healthmaster/ParentSession.java
@@ -0,0 +1,20 @@
+package com.example.healthmaster;
+
+import com.example.healthmaster.model.Child;
+
+public class ParentSession
+{
+    Child child;
+
+    public Child logIn(String parentUsername)
+    {
+        //GET DATA FROM SERVER
+        return null;
+    }
+
+    public Child register(String childUsername, String parentUsername)
+    {
+        //REGISTER AND GET DATA FROM SERVER
+        return null;
+    }
+}
