2c257fac43e9ebbe5d881ce6b6f2db9aa515a3ed
Karolina Piekarz <piekarz.karolina@gmail.com>
2020-02-29 22:51:38 +0100

    part 3

--- health_hero/app/src/main/java/com/example/helathhero/TaskActivity.java
+++ health_hero/app/src/main/java/com/example/helathhero/TaskActivity.java
@@ -2,12 +2,19 @@ package com.example.helathhero;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TaskActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mobile_task);
+        setContentView(R.layout.fragment_task);
+        TextView hed = (TextView)findViewById(R.id.task_header);
+        TextView des = (TextView)findViewById(R.id.task_description);
+
+        hed.setText(new String("ala"));
+        des.setText(new String("ola"));
+
     }
 }
