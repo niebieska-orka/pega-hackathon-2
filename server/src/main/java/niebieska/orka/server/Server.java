5ff5db92076a928a287e5651d696952c890a554c
Aleksandra Drewnik <aleksandra.drewnik@gmail.com>
2020-02-29 14:41:20 +0100

    [S] Set up skeleton server

---
+++ server/src/main/java/niebieska/orka/server/Server.java
@@ -0,0 +1,45 @@
+package niebieska.orka.server;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class Server {
+    private static final String BROKER = "tcp://192.168.43.126:1883";
+    private static final String TOPIC_PREFIX = "niebieska/orka/health/hero";
+    private static final String CHILD_STATUS_REQUEST_TOPIC = TOPIC_PREFIX + "/status/request";
+    private static final String CHILD_STATUS_UPDATE_TOPIC = TOPIC_PREFIX + "/status/update";
+    private static final String CHILD_ACTION_TO_SERVER_TOPIC = TOPIC_PREFIX + "/action/child/server";
+    private static final String CHILD_ACTION_TO_PARENT_TOPIC = TOPIC_PREFIX + "/action/child/parent";
+    private static final String PARENT_ACTION_TO_SERVER_TOPIC = TOPIC_PREFIX + "/action/parent/server";
+
+    //when sending, add "/{ID}" to topic
+    public static void main(String... args) throws Exception {
+        MqttClient client = new MqttClient(BROKER, "niebieska.orka.health.hero.server", new MemoryPersistence());
+        MqttConnectOptions connOpts = new MqttConnectOptions();
+        connOpts.setCleanSession(true);
+        System.out.println("Connecting to broker: " + BROKER);
+        client.connect(connOpts);
+        System.out.println("Connected");
+        client.subscribe(CHILD_STATUS_REQUEST_TOPIC, 1, Server::processChildStatusRequest);
+        client.subscribe(CHILD_ACTION_TO_SERVER_TOPIC, 1, Server::processChildAction);
+        client.subscribe(PARENT_ACTION_TO_SERVER_TOPIC, 1, Server::processParentAction);
+        while (true) {
+            System.out.println("Idling...");
+            Thread.sleep(5000);
+        }
+    }
+
+    private static void processChildStatusRequest(String topic, MqttMessage message) {
+        System.out.println(topic + ": " + message);
+    }
+
+    private static void processChildAction(String topic, MqttMessage message) {
+        System.out.println(topic + ": " + message);
+    }
+
+    private static void processParentAction(String topic, MqttMessage message) {
+        System.out.println(topic + ": " + message);
+    }
+}
