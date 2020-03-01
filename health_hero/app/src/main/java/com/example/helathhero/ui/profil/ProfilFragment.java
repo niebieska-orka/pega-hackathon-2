1ae65a7ce75f44bba06bcbe1d970ca6737610461
Aleksandra Drewnik <aleksandra.drewnik@gmail.com>
2020-03-01 02:38:46 +0100

    added orca picture

--- health_hero/app/src/main/java/com/example/helathhero/ui/profil/ProfilFragment.java
+++ health_hero/app/src/main/java/com/example/helathhero/ui/profil/ProfilFragment.java
@@ -1,12 +1,14 @@
 package com.example.helathhero.ui.profil;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
