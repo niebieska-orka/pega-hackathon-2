1bd3ae0519ccb4a1026caebf8f830e918578d9cc
Karolina Piekarz <piekarz.karolina@gmail.com>
2020-03-01 03:15:51 +0100

    add master app flow

---
+++ health_master/app/src/main/java/com/example/healthmaster/CreateTask.java
@@ -0,0 +1,21 @@
+package com.example.healthmaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateTask extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_task);
+
+
+
+    }
+}
